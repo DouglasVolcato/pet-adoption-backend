4af9019aa71b076763d58f5a5112f99815f08ac9
Douglas Volcato <douglasvolcato@gmail.com>
2023-11-08 00:53:42 -0300

    test: ensure UserRepository is working

---
+++ tests/test-helpers/mocks/entities/user-entity-mock.ts
@@ -0,0 +1,9 @@
+import { UserEntityType } from "../../../../src/domain/protocols";
+import { FakeData } from "../../fake-data";
+
+export const makeUserEntity = (): UserEntityType => ({
+  id: FakeData.id(),
+  name: FakeData.word(),
+  email: FakeData.email(),
+  password: FakeData.password(),
+});
