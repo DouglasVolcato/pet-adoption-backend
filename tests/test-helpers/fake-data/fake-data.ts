a06b11c43ff32480fe055737000709f6e3b45aca
Douglas Volcato <douglasvolcato@gmail.com>
2023-11-08 23:29:40 -0300

    refactor: implement fake data generation

--- tests/test-helpers/fake-data/fake-data.ts
+++ tests/test-helpers/fake-data/fake-data.ts
@@ -3,7 +3,7 @@ import { faker } from "@faker-js/faker";
 export const FakeData = {
   email: (): string => faker.internet.email(),
   password: (): string => faker.internet.password(),
-  word: (length = 10): string => faker.word.sample({ length }),
-  id: (): string => faker.database.mongodbObjectId(),
+  word: (length = 10): string => faker.string.alphanumeric({ length }),
+  id: (): string => faker.string.uuid(),
   numberInteger: (): number => faker.number.int(),
 };
