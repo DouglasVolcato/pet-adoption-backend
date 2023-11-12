bad20495dd854a874b45acd7aea5159770ce9a77
Douglas Volcato <douglasvolcato@gmail.com>
2023-11-12 18:52:20 -0300

    chore: add factories index file

---
+++ src/main/factories/index.ts
@@ -0,0 +1,6 @@
+export * from "./middlewares/auth/user-auth-middleware-factory";
+export * from "./controllers/login/login-controller-factory";
+export * from "./controllers/pet/change-pet-status-controller-factory";
+export * from "./controllers/pet/search-pets-controller-factory";
+export * from "./controllers/pet/index-pets-controller-factory";
+export * from "./controllers/user/create-user-controller-factory";
