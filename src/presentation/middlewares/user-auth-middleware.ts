7b6b5f359553ed919d5458267a9fb8f874618a70
Douglas Volcato <douglasvolcato@gmail.com>
2023-11-12 01:40:39 -0300

    feat: add UserAuthMiddleware

---
+++ src/presentation/middlewares/user-auth-middleware.ts
@@ -0,0 +1,39 @@
+import { GetUserByTokenUserUseCase } from "../../domain/protocols";
+import { MiddlewareInterface } from "../../main/protocols";
+import { ValidatorBuilder } from "../../validation/builders";
+import { ValidatorComposite } from "../../validation/composites";
+import { UnauthorizedError } from "../helpers";
+import { ValidatorInterface } from "../protocols";
+import { Middleware } from "./middleware";
+
+export class UserAuthMiddleware
+  extends Middleware
+  implements MiddlewareInterface
+{
+  private readonly getUserByTokenService: GetUserByTokenUserUseCase.Service;
+
+  public constructor(getUserByTokenService: GetUserByTokenUserUseCase.Service) {
+    super();
+    this.getUserByTokenService = getUserByTokenService;
+  }
+
+  protected async perform(request: any): Promise<any | Error> {
+    const authorizationSplit = request.authorization.split(" ");
+    if (!authorizationSplit || authorizationSplit[0] !== "Bearer") {
+      return new UnauthorizedError();
+    }
+    const foundUser = await this.getUserByTokenService.execute(
+      authorizationSplit[1]
+    );
+    if (!foundUser || foundUser instanceof Error) {
+      return new UnauthorizedError();
+    }
+    return { user: foundUser };
+  }
+
+  protected getValidation(): ValidatorInterface {
+    return new ValidatorComposite([
+      new ValidatorBuilder().of("authorization").isRequired(),
+    ]);
+  }
+}
